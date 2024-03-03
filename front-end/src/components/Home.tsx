6db8d68b0b9074777e02898bd45c6183de0e73ed
RizinGH <rizi.n2021@gmail.com>
2024-03-03 21:51:18 +0530

    - Logout Button Added to the Home Page

--- front-end/src/components/Home.tsx
+++ front-end/src/components/Home.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import '../styles/form.css'
+import { Button } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 
 function Home() {
-    const username  =localStorage.getItem('username')
-
+  const username  =localStorage.getItem('username')
 
   return (
     <div className='homediv'>
       <h2>Login Successful</h2><br />
       <h3>Hi, {username}</h3>
+      <Link to={'/'}>
+      <Button variant='outlined' sx={{color:'white', bgcolor:'#10a37f', '&:hover':{bgcolor:'#0c8769',borderRadius:5}, position:'fixed', top:10, right:10, transition:'.2s'}}>LogOut</Button>
+      </Link>
     </div>
   )
 }
